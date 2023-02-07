7e8c20c5f3d5fb20fc507d9871712acf4371b046
ASerrai <serrai.achraf@gmail.com>
2023-02-07 18:26:04 +0100

    clean layers and address and keep only one for each

--- src/map-module/map.component.ts
+++ src/map-module/map.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
-import {Browser, Map, map, tileLayer, marker} from 'leaflet';
+import {Map, map, tileLayer, marker} from 'leaflet';
 import {AddressService} from "./shared/services/address.service";
 import {Address} from "./shared/models/adresse.model";
 
@@ -45,20 +45,7 @@ export class MapComponent implements OnChanges, AfterViewInit {
                 initialState.zoom
         );
 
-        const isRetina = Browser.retina;
-        const baseUrl =
-                'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey={apiKey}';
-        const retinaUrl =
-                'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}@2x.png?apiKey={apiKey}';
-
-       /* tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-            maxZoom: 30
-        }).addTo(lefletMap);
-
-        */
-
-        var GeoportailFrance_orthos = tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
+        tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
             attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
             bounds: [[-75, -180], [81, 180]],
             minZoom: 2,
@@ -67,19 +54,6 @@ export class MapComponent implements OnChanges, AfterViewInit {
             format: 'image/jpeg',
             style: 'normal'
         } as any).addTo(lefletMap);
-
-        /*tileLayer(isRetina ? retinaUrl : baseUrl, {
-            attribution:
-                    'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | <a href="https://openmaptiles.org/" target="_blank">© OpenMapTiles</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap</a> contributors',
-            // This API key is for use only in stackblitz.com
-            // Get your Geoapify API key on https://www.geoapify.com/get-started-with-maps-api
-            // The Geoapify services is free for small projects and the development phase.
-            apiKey: this.apiKey,
-            maxZoom: 20,
-            id: 'osm-bright',
-        } as any).addTo(lefletMap);
-
-         */
 
         marker([initialState.lat, initialState.lng]).addTo(lefletMap);
     }
--- src/map-module/shared/services/address.service.ts
+++ src/map-module/shared/services/address.service.ts
@@ -1,35 +1,12 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Address, AddressDetails, AddressMinimalDetails} from '../models/adresse.model';
+import {Address, AddressMinimalDetails} from '../models/adresse.model';
 import {Observable, map} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class AddressService {
     constructor(private httpClient: HttpClient) {
     }
-
-    getAddressDetails(address: Address) : Observable<AddressMinimalDetails>{
-        const search = `${address.street} ${address.city} ${address.postalCode} ${address.country}`;
-        return this.httpClient.get<AddressDetails[]>('https://nominatim.openstreetmap.org/search?format=json&q=' + search)
-                .pipe(map(addressList => {
-                    if(addressList && addressList.length !== 0){
-                        return addressList[0];
-                    }
-                    throw Error('No address found for search : ' + search);
-                }));
-    }
-
-    getAddressDetailsGeoApify(address: Address) : Observable<AddressMinimalDetails>{
-        const search = `${address.street} ${address.city} ${address.postalCode} ${address.country}`;
-        return this.httpClient.get<any>('https://api.geoapify.com/v1/geocode/search?apiKey=bbb68c376a6047818a933542a26a4eea&text=' + search)
-                .pipe(map(res => {
-                    if(res && res.features && res.features.length !== 0){
-                        return {lat: res.features[0].properties.lat, lon: res.features[0].properties.lon};
-                    }
-                    throw Error('No address found for search : ' + search);
-                }));
-    }
-
 
     getAddressDetailsGouv(address: Address) : Observable<AddressMinimalDetails>{
         const search = `${address.street} ${address.city} ${address.postalCode} ${address.country}`;
@@ -41,5 +18,4 @@ export class AddressService {
                     throw Error('No address found for search : ' + search);
                 }));
     }
-
 }
